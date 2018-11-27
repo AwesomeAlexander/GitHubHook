c03c529f43b1a7ad29aedb508ac42e2a2a6fc1a6
AwesomeAlexander <alexander.m.ng@gmail.com>
2018-11-26 22:43:03 -0800

    Set up Hook Responder

---
+++ hookresponder.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DEFAULT_RESPONSE="HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n"
+CUR=$(pwd)
+
+while true; do
+	HOOK=$(echo -en ${RESPONSE:-$DEFAULT_RESPONSE} | nc -l ${PORT:-9000} | head -n 4)
+
+	if echo $HOOK | sed -n '
+		1s/^POST \/.+ HTTP\/1.1$//p
+		4s/^GitHub-Hookshot\/\.+$//p
+	' | test; then
+		# Pull to the corresponding repository
+		DIR=$(hook | head -n 1 | awk '$2' | sed -e 's/\.\.//')
+		cd "$CUR$DIR"
+		&& git pull
+		|| echo "An error occured while you attempted to 'git pull' from the
+		directory '$DIR'"
+	fi
+done
