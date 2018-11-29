36c906d0cf9b473d8a6b0902291334ff065367c3
AwesomeAlexander <alexander.m.ng@gmail.com>
2018-11-28 16:21:33 -0800

    Prettified

--- hookresponder.sh
+++ hookresponder.sh
@@ -4,17 +4,21 @@ DEFAULT_RESPONSE="HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n"
 CUR=$(pwd)
 
 while true; do
-	HOOK=$(echo -en ${RESPONSE:-$DEFAULT_RESPONSE} | nc -l ${PORT:-9000} | head -n 4)
+	HOOK=$( echo -en ${RESPONSE:-$DEFAULT_RESPONSE} | nc -l ${PORT:-9000} )
+	
+	echo "Recieved HTTP Request:\n$HOOK"
 
 	if echo $HOOK | sed -n '
 		1s/^POST \/.+ HTTP\/1.1$//p
 		4s/^GitHub-Hookshot\/\.+$//p
 	' | test; then
 		# Pull to the corresponding repository
-		DIR=$(hook | head -n 1 | awk '$2' | sed -e 's/\.\.//')
+		DIR=$(echo $HOOK | head -n 1 | awk '$2' | sed -e 's/\.\.//' )
 		cd "$CUR$DIR" \
 			&& git pull \
 			&& echo "Successfully 'git pull'ed from $PWD" \
 			|| echo "An error occured while you attempted to 'git pull' from the directory '$DIR'"
 	fi
+
+	echo "\n\n----------------------------------------------------\n\n"
 done
